96e2f29d16f893b28eae8b733d39e7d4374adaae
Joshua <josuealejandrof926@gmail.com>
2024-06-23 15:26:29 +0200

    Tailwind Config Setup, with colors theme and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-light': '#ffb4f7',
+        primary: '#ffa1f5',
+        'primary-dark': '#cc81c4',
+        'secondary-light': '#c995fa',
+        secondary: '#bc7af9',
+        'secondary-dark': '#9662c7',
+        'accent-light': '#f9ffaa',
+        accent: '#F8FF95',
+        'accent-dark': '#c6cc77',
+      },
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
