c4402d3c0bd5b81bcfbdc7bc5e749eebf7ab4800
Joshua <josuealejandrof926@gmail.com>
2024-06-24 14:55:14 +0200

    Progressing With home grid

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,10 @@
 const Card = ({ item }) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-200 max-w-56 aspect-square p-4 rounded-2xl">
-      <img className="flex-grow aspect-square object-cover p-2 rounded-2xl" src={item.image} alt="Product Image" /> 
-      <h1 className="text-sm">{item.title}</h1>
+    <div className="flex flex-col items-center justify-center bg-gray-200 w-56 h-72 p-4 rounded-2xl flex-shrink-0">
+      <img className="flex-grow aspect-square object-cover rounded-2xl" src={item.image} alt="Product Image" />
+      <div className="flex flex-col justify-center items-center gap-2 text-center h-12">
+        <h1 className="text-sm text-ellipsis overflow-hidden ">{item.title}</h1>
+      </div>
     </div>
   );
 };
