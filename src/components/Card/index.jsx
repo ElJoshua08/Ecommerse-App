fd0dbfe8d4038421d6d8ad1d5216560a773ed37d
Joshua <josuealejandrof926@gmail.com>
2024-06-25 14:49:11 +0200

    Install react icons

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,12 @@
+import { FaRegStar, FaStar } from "react-icons/fa";
+import { MdStarOutline } from "react-icons/md";
+
 const Card = ({ item }) => {
   return (
     // Card
     <div className="flex flex-row items-center justify-between bg-gray-200 w-5/12 min-w-96 h-48 rounded-lg flex-shrink-0 flex-grow shadow-md hover:shadow-2xl transition-shadow duration-300 ease-in-out cursor-pointer">
       {/* Card Image and Category */}
-      <div className="overflow-hidden h-full w-2/5 flex flex-col justify-center items-start relative">
+      <div className="overflow-hidden flex-shrink-0  h-full w-2/5 flex flex-col justify-center items-start relative">
         <img
           className="flex-grow aspect-video h-full object-cover object-center rounded-lg"
           src={item.image}
@@ -15,10 +18,16 @@ const Card = ({ item }) => {
       </div>
 
       {/* Card Content */}
-      <div className="flex flex-row justify-center items-center gap-2 text-center h-12">
-        <h1 className="text-sm font-bold truncate max-w-[20ch]">
+      <div className="flex flex-grow flex-col justify-around items-center gap-2 text-center h-12 overflow-hidden">
+        {/* Card Title */}
+        <h1 className="text-sm font-bold max-w-[40ch] text-wrap text-ellipsis overflow-hidden text-left ml-2">
           {item.title}
         </h1>
+        {/* Card Rate and Count */}
+        <div className="flex flex-row justify-center gap-1 items-center">
+          <FaRegStar />
+          <FaStar /> 
+        </div>
       </div>
     </div>
   );
