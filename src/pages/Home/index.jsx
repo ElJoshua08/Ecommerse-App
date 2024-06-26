e4427c7c019aa6f79f6bc7c875271ba8fc8bf0d6
Joshua <josuealejandrof926@gmail.com>
2024-06-27 01:08:12 +0200

    Custom scrollbar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,27 +5,25 @@ const Home = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    console.log('fetching data');
     async function fetchData() {
       const res = await fetch('https://fakestoreapi.com/products');
       const data = await res.json();
 
       setItems(data);
-      console.log(data[0]);
     }
 
     fetchData();
   }, []);
 
   return (
-    <main className="flex-grow flex items-center justify-center flex-wrap gap-4 w-full mt-[70px] min-h-screen top-70 relative p-5">
+    <>
       {items.map((item) => (
         <Card
           key={item.id}
           item={item}
         />
       ))}
-    </main>
+    </>
   );
 };
 
--- src/pages/MyAccount/index.jsx
+++ src/pages/MyAccount/index.jsx
@@ -1,12 +1,9 @@
-import Header from '@/components/Header';
 
 const MyAccount = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div>
-        <h1>My Account</h1>
-      </div>
-    </div>
+    <>
+      <h1>My Account</h1>
+    </>
   );
 };
 
