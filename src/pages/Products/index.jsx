9fd57692ddef4cb15ba074eea8063b88aba8be0b
Joshua <josuealejandrof926@gmail.com>
2024-06-28 01:50:11 +0200

    Add Loading

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -11,13 +11,19 @@ const Products = () => {
   ];
   const [items, setItems] = useState([]);
   const [filter, setFilter] = useState('all');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
+      setLoading(true);
       const res = await fetch('https://fakestoreapi.com/products');
       const data = await res.json();
 
-      setItems(data);
+      // Simulate a delay to ensure loading animation is visible
+      setTimeout(() => {
+        setItems(data);
+        setLoading(false);
+      }, 2000); // 2 seconds delay
     }
 
     fetchData();
@@ -32,15 +38,15 @@ const Products = () => {
   return (
     <>
       {/* Filters */}
-      <div className="flex flex-row justify-left items-center gap-4 h-12 w-full rounded-2xl">
+      <div className="flex flex-row justify-left items-center gap-4 h-12 w-full rounded-2xl mb-4">
         {categories.map((category, index) => (
           <button
             key={index}
             className={`${
               filter === category
-                ? 'bg-primary'
-                : 'bg-gray-200 hover:bg-gray-300 hover:text-gray-700'
-            } text-gray-500 font-medium text-normal rounded-md px-4 py-2 text-sm transition`}
+                ? 'bg-primary text-gray-700'
+                : 'bg-gray-200 text-gray-500 hover:bg-gray-300 hover:text-gray-700'
+            } font-medium text-normal rounded-md px-4 py-2 text-sm transition`}
             onClick={() => handleFilter(category)}
           >
             {category}
@@ -48,23 +54,29 @@ const Products = () => {
         ))}
       </div>
 
-      {/* Products wrapper for display */}
-      <div className="flex flex-wrap flex-row justify-center gap-4 items-start w-full">
-        {/* Products */}
-        {items.map((item) =>
-          filter === 'all' ? (
-            <Card
-              key={item.id}
-              item={item}
-            />
-          ) : item.category === filter ? (
-            <Card
-              key={item.id}
-              item={item}
-            />
-          ) : null
-        )}
-      </div>
+      {/* Loading Animation */}
+      {loading ? (
+        <div className="flex justify-center items-center w-full h-64">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
+        </div>
+      ) : (
+        <div className="flex flex-wrap flex-row justify-center gap-4 items-start w-full">
+          {/* Products */}
+          {items.map((item) =>
+            filter === 'all' ? (
+              <Card
+                key={item.id}
+                item={item}
+              />
+            ) : item.category === filter ? (
+              <Card
+                key={item.id}
+                item={item}
+              />
+            ) : null
+          )}
+        </div>
+      )}
     </>
   );
 };
