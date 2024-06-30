0fade87debc09df95c1737cf516dde315029d5de
Joshua <josuealejandrof926@gmail.com>
2024-06-30 16:35:09 +0200

    Change zindex of header for overflow

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = ({ actualPage, setActualPage }) => {
   return (
-    <header className="flex bg-slate-100 shadow-md shadow-slate-200 py-4 px-2 fixed top-0 left-0 w-full items-center justify-between h-[70px] dark:bg-gray-800 dark:shadow-gray-700 z-10">
+    <header className="flex bg-slate-100 shadow-md shadow-slate-200 py-4 px-2 fixed top-0 left-0 w-full items-center justify-between h-[70px] dark:bg-gray-800 dark:shadow-gray-700 z-[90]">
       <div className="flex items-center gap-2">
         <img
           src="/logo/1024.png"
