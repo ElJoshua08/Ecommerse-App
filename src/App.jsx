e87b0cc197057063fb738f8d49fd62081791dac6
Joshua <josuealejandrof926@gmail.com>
2024-06-30 04:05:56 +0200

    Card dark theme styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from '@/components/Footer';
 
 function App() {
   return (
-    <div className="App bg-white dark:bg-gray-900 dark:text-white relative overflow-auto">
+    <div className="App bg-white dark:bg-gray-900 dark:text-white relative overflow-auto min-h-screen">
       <Router>
         <Header />
         <div className="flex-grow flex items-start justify-center flex-wrap gap-4 w-full mt-[85px] relative px-5 py-3 mb-14">
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -19,7 +19,7 @@ const Card = ({ item }) => {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-between bg-slate-200 w-96 max-w-[400px] min-w-52 h-48 rounded-lg flex-shrink-0 flex-grow shadow-md hover:shadow-2xl transition-shadow duration-300 ease-in-out cursor-pointer">
+    <div className="flex flex-row items-center justify-between bg-slate-200 w-96 max-w-[400px] min-w-52 h-48 rounded-lg flex-shrink-0 flex-grow shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out cursor-pointer dark:bg-slate-800 dark:shadow-slate-600/50 dark:hover:shadow-slate-600/50">
       {/* Card Image and Category */}
       <div className="overflow-hidden flex-shrink-0 h-full w-2/5 flex flex-col justify-center items-start relative">
         <img
@@ -27,7 +27,7 @@ const Card = ({ item }) => {
           src={item.image}
           alt="Product Image"
         />
-        <p className="absolute bottom-1 right-1 z-[10] text-sm letter text-white bg-slate-700/30 backdrop-blur-md rounded-full px-2 py-1">
+        <p className="absolute bottom-1 right-1 z-[10] text-sm tracking-wide text-white bg-slate-800/70 backdrop-blur-lg rounded-lg px-2 py-1">
           {item.category}
         </p>
       </div>
@@ -35,7 +35,7 @@ const Card = ({ item }) => {
       {/* Card Content */}
       <div className="flex flex-grow flex-col justify-start items-start text-center overflow-hidden h-full p-2 relative">
         {/* Card Title */}
-        <h1 className="text-md font-semibold text-wrap text-ellipsis overflow-hidden text-left leading-tight h-fit max-h-[2lh]">
+        <h1 className="text-md font-semibold text-wrap text-ellipsis overflow-hidden text-left leading-tight h-fit max-h-[2lh] dark:text-slate-100">
           {item.title}
         </h1>
         {/* Card Rate and Count */}
@@ -43,23 +43,23 @@ const Card = ({ item }) => {
           {stars.map((star, index) => (
             <span key={index}>{star}</span>
           ))}
-          <p className="text-sm text-gray-500 flex items-center h-full">
+          <p className="text-sm text-slate-700 flex items-center h-full dark:text-slate-400">
             {item.rating.count} reviews
           </p>
         </div>
 
         {/* Description */}
-        <p className="text-xs text-gray-500 text-ellipsis overflow-hidden text-left leading-tight mt-1 h-fit max-h-[4lh]">
+        <p className="text-xs text-slate-6100 text-ellipsis overflow-hidden text-left leading-tight mt-1 h-fit max-h-[4lh] dark:text-slate-300">
           {item.description}
         </p>
 
         {/* Price */}
-        <p className="text-sm text-gray-500 absolute bottom-2 left-2">
+        <p className="text-md text-gray-500 absolute bottom-1 left-2 font-bold dark:text-slate-300">
           {item.price} €
         </p>
 
         {/* Add to cart button */}
-        <button className="bg-primary-dark text-white rounded-lg px-4 py-2 text-sm right-[0.6rem] bottom-2 absolute font-semibold transition hover:bg-primary">
+        <button className="bg-secondary text-slate-100 rounded-md px-2 py-1 text-md tracking-wide right-[0.6rem] bottom-2 absolute font-semibold transition hover:bg-secondary-dark">
           Add to cart
         </button>
       </div>
