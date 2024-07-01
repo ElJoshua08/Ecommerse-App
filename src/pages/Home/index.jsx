373385f8749bba15a7e85bdc452d82b3c5afd3dc
Joshua <josuealejandrof926@gmail.com>
2024-07-01 20:39:30 +0200

    Fix why choose ShopNet Wave

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -56,13 +56,13 @@ const Home = () => {
         </section>
 
         {/* Why choose ShopNet */}
-        <section className="flex flex-col items-center justify-center gap-4 w-full min-h-screen flex-shrink-0 bg-slate-200 relative pt-16 mt-4 dark:bg-slate-800">
+        <section className="flex flex-col items-center justify-center gap-4 w-full min-h-screen flex-shrink-0 bg-slate-200 relative pt-16 mt-4 dark:bg-slate-800 0">
           <img
             src={`/waves/${
               theme ? (theme == 'dark' ? 'dark' : 'light') : 'dark'
             }/homeSection2.png`}
             alt="wave"
-            className="absolute top-[-70px] left-0 w-full z-[1]"
+            className="absolute top-0 translate-y-[-97%] left-0 w-full z-[1]"
           />
           <h2 className="text-center text-2xl font-medium text-slate-400 mb-4">
             Why choose ShopNet?
