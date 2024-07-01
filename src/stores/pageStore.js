004372854ee48c5e98cc1916c248240367292aa0
Joshua <josuealejandrof926@gmail.com>
2024-07-02 01:58:13 +0200

    Change header icons

--- src/stores/pageStore.js
+++ src/stores/pageStore.js
@@ -1,12 +1,11 @@
-import { FaHome, FaShoppingCart, FaUser } from 'react-icons/fa';
-import { FaShop } from 'react-icons/fa6';
+import { FaHome, FaShoppingBasket, FaShoppingCart, FaUser } from 'react-icons/fa';
 import { create } from 'zustand';
 
 const usePageStore = create((set) => ({
   pages: [
     { name: 'Home', path: '/', icon: FaHome},
-    { name: 'Products', path: '/products', icon: FaShop },
-    { name: 'Cart', path: '/cart', icon: FaShoppingCart },
+    { name: 'Products', path: '/products', icon: FaShoppingCart },
+    { name: 'Cart', path: '/cart', icon: FaShoppingBasket },
     { name: 'My Account', path: '/my-account', icon: FaUser },
   ],
   actualPage: '/',
