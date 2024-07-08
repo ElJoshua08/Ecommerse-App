b89bc027ed0f58918753b718ecb4c53600cad1ed
Joshua <josuealejandrof926@gmail.com>
2024-07-08 20:11:23 +0200

    Change shop now button in home page

--- src/pages/Home/_components/Main/index.jsx
+++ src/pages/Home/_components/Main/index.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 const Main = () => {
   return (
     // Main Content
-    <section className="flex flex-row items-center justify-around min-h-[calc(100vh-70px)] w-full relative px-5 *:z-20">
-      <h1 className="flex-grow text-center text-6xl text-slate-600 dark:text-slate-200 max-w-xl">
+    <section className="relative flex min-h-[calc(100vh-70px)] w-full flex-row items-center justify-around px-5 *:z-20">
+      <h1 className="max-w-xl flex-grow text-center text-6xl text-slate-600 dark:text-slate-200">
         WELCOME TO{' '}
-        <strong className="animate-wave bg-[length:200%_100%] text-transparent bg-clip-text bg-gradient-to-r from-primary-dark to-amber-400 text-7xl">
+        <strong className="animate-wave bg-gradient-to-r from-primary-dark to-amber-400 bg-[length:200%_100%] bg-clip-text text-7xl text-transparent">
           SHOP NET
         </strong>{' '}
         <br /> YOUR FAVORITE SHOPPING APP
       </h1>
-      <section className="flex flex-col items-center justify-center gap-4 w-2/5 flex-shrink-0">
+      <section className="flex w-2/5 flex-shrink-0 flex-col items-center justify-center gap-4">
         {/* Logo Image */}
         <img
           src="/logo/1024.png"
           alt="logo"
-          className="w-full rounded-lg aspect-square p-8 drop-shadow-card"
+          className="aspect-square w-full rounded-lg p-8 drop-shadow-card"
         />
-        <h3 className="relative top-[-60px] text-center text-xl text-slate-500 font-light z-10">
+        <h3 className="relative top-[-60px] z-10 text-center text-xl font-light text-slate-500">
           <strong className="text-slate-600 dark:text-slate-200">Fast</strong> -{' '}
           <strong className="text-slate-600 dark:text-slate-200">
             Affordable
@@ -29,12 +29,20 @@ const Main = () => {
       </section>
 
       <Link to="products">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-3xl py-2 px-3 rounded absolute bottom-8 left-[50%] translate-x-[-50%] transition-colors duration-200 z-[99]">
-          SHOP NOW
-        </button>
+        <div class="group inline-flex absolute left-[50%] translate-x-[-50%] bottom-8 h-12">
+          <div class="transitiona-all animate-tilt absolute -inset-px rounded-xl bg-gradient-to-r from-primary-light via-[#FF44EC] to-accent-dark opacity-70 blur-lg duration-1000 group-hover:-inset-1 group-hover:opacity-100 group-hover:duration-200"></div>
+          <a
+            href="#"
+            title="Get quote now"
+            class="font-pj relative inline-flex items-center justify-center rounded-xl bg-slate-600 dark:bg-purple-600 px-8 py-2 text-2xl font-bold text-white transition-all duration-200 hover:text-primary-light"
+            role="button"
+          >
+            Shop Now!
+          </a>
+        </div>
       </Link>
     </section>
-  );
+  )
 };
 
 export { Main };
